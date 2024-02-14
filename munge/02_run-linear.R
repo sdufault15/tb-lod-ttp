b179af327d347712608101c6926f03d66dabe13c
Suzanne Dufault <sdufault@som-9v6qj02-dt.ucsf.edu>
2024-02-14 09:20:36 -0800

    adding study 29 linear model

--- munge/02_run-linear.R
+++ munge/02_run-linear.R
@@ -156,3 +156,51 @@ m_linear_nc002_25 <- brm(log10(dtp_25) | cens(censored_25) ~ weeks + (1 + weeks 
 save(m_linear_nc002_25,
      file = here("data", "model-generated", 
                  paste0(Sys.Date(), "_linear-nc002-lod-25.RData")))
+
+###########################
+# Run the models on Study 29
+###########################
+load(here("data", "cleaned-data", "2024-02-14_TBTC-S29-clean.RData"))
+# Model took 4.4 hours to run locally
+m_linear_s29_42 <- brm(log10(dtp_42) | cens(censored_42) ~ weeks + (1 + weeks | patient.id + ACTARM), # run the model
+                         data = df_analysis_s29,
+                         init = 0,
+                         chains = 4,
+                         control = list(adapt_delta = 0.99),
+                         iter = 4000,
+                         backend = "cmdstanr", # attempt to improve convergence speed
+                         normalize = FALSE, # attempt to improve speed of convergence (https://discourse.mc-stan.org/t/faster-convergence/21532)
+                         prior = prior(normal(0,4), class = "Intercept"))
+save(m_linear_s29_42,
+     file = here("data", "model-generated", 
+                 paste0(Sys.Date(), "_linear-s29-lod-42.RData")))
+rm(m_linear_s29_42)
+beepr::beep()
+
+m_linear_s29_30 <- brm(log10(dtp_30) | cens(censored_30) ~ weeks + (1 + weeks | patient.id + ACTARM), # run the model
+                         data = df_analysis_s29,
+                         init = 0,
+                         chains = 4,
+                         control = list(adapt_delta = 0.99),
+                         iter = 4000,
+                         backend = "cmdstanr", # attempt to improve convergence speed
+                         normalize = FALSE, # attempt to improve speed of convergence (https://discourse.mc-stan.org/t/faster-convergence/21532)
+                         prior = prior(normal(0,4), class = "Intercept"))
+
+save(m_linear_s29_30,
+     file = here("data", "model-generated", 
+                 paste0(Sys.Date(), "_linear-s29-lod-30.RData")))
+
+m_linear_s29_25 <- brm(log10(dtp_25) | cens(censored_25) ~ weeks + (1 + weeks | patient.id + ACTARM), # run the model
+                         data = df_analysis_s29,
+                         init = 0,
+                         chains = 4,
+                         control = list(adapt_delta = 0.99),
+                         iter = 4000,
+                         backend = "cmdstanr", # attempt to improve convergence speed
+                         normalize = FALSE, # attempt to improve speed of convergence (https://discourse.mc-stan.org/t/faster-convergence/21532)
+                         prior = prior(normal(0,4), class = "Intercept"))
+
+save(m_linear_s29_25,
+     file = here("data", "model-generated", 
+                 paste0(Sys.Date(), "_linear-s29-lod-25.RData")))
